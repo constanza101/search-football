bf61273ea86d4442c496490a02c00437f31672b8
Constanza Díaz <constanza.diaz@gmail.com>
2019-04-13 16:47:00 +0200

    if statement in landing-page.ts

--- frontend/ligaEsp/src/app/landing-page/landing-page.component.ts
+++ frontend/ligaEsp/src/app/landing-page/landing-page.component.ts
@@ -16,19 +16,27 @@ export class LandingPageComponent implements OnInit {
   getResults(){
     var teamA = $("#inputTeam1").val();
     var teamB = $("#inputTeam2").val();
+    console.log("teamA "+teamA)
+    console.log("teamB "+teamB)
     var stringteamA = teamA.toString()
     var stringteamB = teamB.toString()
-
     var team1 = stringteamA.replace(" ", "+");
-    var team2 = stringteamA.replace(" ", "+");
-    console.log(team1, team2)
+    var team2 = stringteamB.replace(" ", "+");
+    console.log("teamA "+team1)
+    console.log("teamB "+team2)
 
-    var urlTeams = "http://localhost:8000/matches/"+team1+"/"+team2;
-    console.log(urlTeams)
-    $.get(urlTeams, (response) => {
-      alert(urlTeams)
-      console.log(response)
-      })
+    if (team1 != "" && team2 != "") {
+      var urlTeams = "http://localhost:8000/matches/"+team1+"/"+team2;
+      //console.log(urlTeams)
+      $.get(urlTeams, (response) => {
+        alert(urlTeams)
+        console.log(response)
+        })
+    }
+    else if (team1 == ""){console.log("no hay team1")}
+    else if (team2 == ""){console.log("no hay team2")}
+    else{console.log("no sabemos que está fallando")}
+
   }
 
 
